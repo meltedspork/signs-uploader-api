f166ea63453442e92fb2e68154a2f675580a3811
Jason Salerno <jason@meltedspork.com>
2021-03-27 17:27:28 -0500

    fix private key parsing

--- environments/firebase.admin.config.js
+++ environments/firebase.admin.config.js
@@ -7,7 +7,7 @@ module.exports = {
   /* END Firebase Client */
   TYPE: process.env.FIREBASE_ADMIN_TYPE,
   PRIVATE_KEY_ID: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
   CLIENT_EMAIL: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
   CLIENT_ID: process.env.FIREBASE_ADMIN_CLIENT_ID,
   AUTH_URI: process.env.FIREBASE_ADMIN_AUTH_URI,
