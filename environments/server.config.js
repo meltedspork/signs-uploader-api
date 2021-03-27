ef6f4d65eb6db653e84216d4c49d726c1bc22811
Jason Salerno <jason@meltedspork.com>
2021-03-27 17:34:31 -0500

    support heroku env for $PORT

--- environments/server.config.js
+++ environments/server.config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   ORIGINS: process.env.SERVER_ORIGINS.split(','),
-  PORT: process.env.SERVER_PORT,
+  PORT: process.env.SERVER_PORT || PORT,
   PRODUCTION: (process.env.SERVER_ENVIRONMENT === 'production'),
 };
