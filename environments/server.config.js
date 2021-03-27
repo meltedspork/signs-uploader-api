40daa30c2486e9e36b06a85ed38fc4c9f6d8235d
Jason Salerno <jason@meltedspork.com>
2021-03-27 17:35:49 -0500

    tweak PORT logic

--- environments/server.config.js
+++ environments/server.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  ORIGINS: process.env.SERVER_ORIGINS.split(','),
-  PORT: process.env.SERVER_PORT || PORT,
-  PRODUCTION: (process.env.SERVER_ENVIRONMENT === 'production'),
+  ORIGINS: process.env.ORIGINS.split(','),
+  PORT: process.env.PORT,
+  PRODUCTION: (process.env.ENVIRONMENT === 'production'),
 };
