847e80306808e091be8c322b876168f2c21bfeed
Jason Salerno <jason@meltedspork.com>
2021-03-27 17:57:08 -0500

    fix port with silly typo

--- environments/server.config.js
+++ environments/server.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  ORIGINS: process.env.ORIGINS.split(','),
+  ORIGINS: process.env.SERVER_ORIGINS.split(','),
   PORT: process.env.PORT,
-  PRODUCTION: (process.env.ENVIRONMENT === 'production'),
+  PRODUCTION: (process.env.SERVER_ENVIRONMENT === 'production'),
 };
