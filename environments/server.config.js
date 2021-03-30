2a8b7cd1d48bb586dd3b24f9c38e4b12bd5f4a09
Jason Salerno <jason@meltedspork.com>
2021-03-29 20:29:33 -0500

    update server config

--- environments/server.config.js
+++ environments/server.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+  DATABASE_URL: process.env.DATABASE_URL,
   ORIGINS: process.env.SERVER_ORIGINS.split(','),
   PORT: process.env.PORT,
   PRODUCTION: (process.env.SERVER_ENVIRONMENT === 'production'),
