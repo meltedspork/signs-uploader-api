00ef70f6e992cbe94b6637bc1d47aa243050b41b
Jason Salerno <jason@meltedspork.com>
2021-04-01 16:40:06 -0500

    addIndex for uid and etc

--- migrations/20210330194732-create-sign.js
+++ migrations/20210330194732-create-sign.js
@@ -3,47 +3,59 @@ const { DataTypes } = require('sequelize');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Signs', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      uid: {
-        allowNull: false,
-        type: Sequelize.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        unique: true,
-      },
-      title: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      pronounce: {
-        type: Sequelize.STRING,
-      },
-      definition: {
-        type: Sequelize.TEXT,
-      },
-      state: {
-        allowNull: false,
-        type: Sequelize.ENUM,
-        values: [
-          'created',
-          'drafted',
-          'published',
-        ],
-        defaultValue: 'created',
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      }
-    });
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.createTable('Signs', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        uid: {
+          allowNull: false,
+          type: Sequelize.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          unique: true,
+        },
+        title: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        pronounce: {
+          type: Sequelize.STRING,
+        },
+        definition: {
+          type: Sequelize.TEXT,
+        },
+        state: {
+          allowNull: false,
+          type: Sequelize.ENUM,
+          values: [
+            'created',
+            'drafted',
+            'published',
+          ],
+          defaultValue: 'created',
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        }
+      }, { transaction });
+
+      await queryInterface.addIndex('Signs', ['uid'], { transaction });
+
+      await transaction.commit();
+    } catch (err) {
+      console.log('Rolling back: ', err);
+      await transaction.rollback();
+      throw err;
+    }
   },
 };
