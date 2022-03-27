621113439838b8b71ca4c18e523d5ab7f8dd1c1f
Jason Salerno <jason@meltedspork.com>
2022-03-27 18:04:08 -0500

    wip

--- __tests__/__graphql__/__resolvers__/__queries__/sign.test.js
+++ __tests__/__graphql__/__resolvers__/__queries__/sign.test.js
@@ -8,6 +8,8 @@
 //   uid: 'foobar-001',
 // };
 
+
+
 // let obj = {
 //   models: {
 //     Sign: {
@@ -33,111 +35,111 @@
 //   });
 // });
 
-// const { esClient } = require('../../../config/elasticsearch.config');
-// const { signUrl } = require('../../../services/aws-s3-sign');
+// // const { esClient } = require('../../../config/elasticsearch.config');
+// // const { signUrl } = require('../../../services/aws-s3-sign');
 
-// const signQueries = {
-//   async viewSign (_root, { uid }, { session, models, user }) {
-//     const {
-//       definition,
-//       name,
-//       pronounce,
-//       state,
-//       topics,
-//       videos,
-//     } = await models.Sign.findOne({
-//       where: { uid },
-//       include: [
-//         'topics',
-//         'videos',
-//       ],
-//     });
+// // const signQueries = {
+// //   async viewSign (_root, { uid }, { session, models, user }) {
+// //     const {
+// //       definition,
+// //       name,
+// //       pronounce,
+// //       state,
+// //       topics,
+// //       videos,
+// //     } = await models.Sign.findOne({
+// //       where: { uid },
+// //       include: [
+// //         'topics',
+// //         'videos',
+// //       ],
+// //     });
 
-//     videoUrls = videos.map((video) => {
-//       const {
-//         metadata: {
-//           key,
-//         }
-//       } = video;
-//       const fileName = key.split('.')[0];
-//       return signUrl(`${fileName}.gif`);
-//     });
+// //     videoUrls = videos.map((video) => {
+// //       const {
+// //         metadata: {
+// //           key,
+// //         }
+// //       } = video;
+// //       const fileName = key.split('.')[0];
+// //       return signUrl(`${fileName}.gif`);
+// //     });
 
-//     const fileName = '795eb62a-696e-4973-857d-8fc28be480cf';
-//     const videoUrl = signUrl(`${fileName}_k8s.gif`);
-//     videoUrls.push(videoUrl);
-//     console.log('-------______videoUrls', videoUrls);
+// //     const fileName = '795eb62a-696e-4973-857d-8fc28be480cf';
+// //     const videoUrl = signUrl(`${fileName}_k8s.gif`);
+// //     videoUrls.push(videoUrl);
+// //     console.log('-------______videoUrls', videoUrls);
 
-//     const allTopics = await models.Topic.findAll();
+// //     const allTopics = await models.Topic.findAll();
 
-//     const signForm = {
-//       sign: {
-//         name,
-//         definition,
-//         pronounce,
-//         state,
-//         topics, //: topics.map(({ name, value }) => ({ name, value, })).flat(),
-//         videoUrls,
-//       },
-//       topics: allTopics,
-//     };
-//     console.log('signForm:', signForm);
+// //     const signForm = {
+// //       sign: {
+// //         name,
+// //         definition,
+// //         pronounce,
+// //         state,
+// //         topics, //: topics.map(({ name, value }) => ({ name, value, })).flat(),
+// //         videoUrls,
+// //       },
+// //       topics: allTopics,
+// //     };
+// //     console.log('signForm:', signForm);
 
-//     console.log('------->>>>> session', session);
-//     return signForm;
-//   },
-//   async viewSigns (_root, args, { res }) {
-//     const {
-//       page = 1,
-//       size: limit = 15,
-//     } = args;
-//     const offset = (page - 1) * limit;
+// //     console.log('------->>>>> session', session);
+// //     return signForm;
+// //   },
+// //   async viewSigns (_root, args, { res }) {
+// //     const {
+// //       page = 1,
+// //       size: limit = 15,
+// //     } = args;
+// //     const offset = (page - 1) * limit;
   
-//     const {
-//       body: {
-//         hits,
-//       }
-//     } = await esClient.search({
-//       body: {
-//         from: offset,
-//         size: limit,
-//         query: { match_all: {}},
-//         sort: [
-//           { name: { order: 'asc' }},
-//         ],
-//       }
-//     });
-//     console.log('hits:', hits);
-//     const {
-//       total,
-//       hits: results,
-//     } = hits;
-//     const signs = results.map(({
-//       _id: uid,
-//       _source: {
-//         name,
-//         state,
-//       }
-//     }) => {
-//       return {
-//         uid,
-//         name,
-//         state,
-//       }
-//     });
-//     console.log('signs:', signs);
+// //     const {
+// //       body: {
+// //         hits,
+// //       }
+// //     } = await esClient.search({
+// //       body: {
+// //         from: offset,
+// //         size: limit,
+// //         query: { match_all: {}},
+// //         sort: [
+// //           { name: { order: 'asc' }},
+// //         ],
+// //       }
+// //     });
+// //     console.log('hits:', hits);
+// //     const {
+// //       total,
+// //       hits: results,
+// //     } = hits;
+// //     const signs = results.map(({
+// //       _id: uid,
+// //       _source: {
+// //         name,
+// //         state,
+// //       }
+// //     }) => {
+// //       return {
+// //         uid,
+// //         name,
+// //         state,
+// //       }
+// //     });
+// //     console.log('signs:', signs);
 
-//     res.header('X-Pagination-Total', total.value);
-//     res.header('X-Pagination-Page', page);
-//     res.header('X-Pagination-Size', limit);
+// //     res.header('X-Pagination-Total', total.value);
+// //     res.header('X-Pagination-Page', page);
+// //     res.header('X-Pagination-Size', limit);
 
-//     return signs;
+// //     return signs;
 
-//     // NOTE: SQL query as backup
-//     // const { count, rows } = await models.Sign.findAndCountAll({ offset, limit, order: [['name', 'ASC']] });
-//     // res.header('X-Pagination-Total', count);
-//     // return rows;
-//   }
-// };
+// //     // NOTE: SQL query as backup
+// //     // const { count, rows } = await models.Sign.findAndCountAll({ offset, limit, order: [['name', 'ASC']] });
+// //     // res.header('X-Pagination-Total', count);
+// //     // return rows;
+// //   }
+// // };
 
-// module.exports = signQueries;
+// // module.exports = signQueries;
